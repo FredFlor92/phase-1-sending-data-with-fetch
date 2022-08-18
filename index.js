a35901b705a7e25debdcdb788c8f6905984436be
Freddy Florial <freddyflorial@gmail.com>
2022-08-18 19:21:46 -0400

    commit tree 74d7e819f4cb2801232fa465f7ec4f0efacab886

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+// makes a POST request to /users with a name and email
+function submitData(userName, userEmail){
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        })
+    })
+    .then(function (response){
+        return response.json()
+    })
+    .then(function (object){
+        // console.log(object);
+        document.body.innerHTML = object["id"]
+    })
+    // handles a failed POST request using catch, appends the error message to the DOM
+    .catch(function (error){
+        document.body.innerHTML = error.message
+    })
+}
